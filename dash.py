b164db89bf36ab601731d29f40d25376934d08fc
manylayers <domatthew1854@gmail.com>
2022-03-05 04:01:09 -0800

    Working on object oriented approach

--- dash.py
+++ dash.py
@@ -6,36 +6,86 @@ from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from secret import USERNAME, PASSWORD, SERVER_ID
-
+from secret import USERNAME, PASSWORD, SERVER_ID, HEADERS
 import json
+import requests
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
+class TownshipLogger():
+    def __init__(self):
+        
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+        self.driver = webdriver.Chrome(PATH, options = options)        
+        self.player_list = []
+        
+    def ping_server(self):
+        
+        url = "https://967phuchye.execute-api.ap-southeast-2.amazonaws.com/prod/api/servers/2004963217"
+    
+        try:
+            response = requests.request("GET", url, headers=HEADERS, params={"limit":"20"})
+            
+            if(response.status_code == 200):
+                return response.json()
+        
+        except e:
+            print(e)
+            return None
+        
+    def authenticate(self):
+
+        self.driver.get("http://dash.townshiptale.com")
+
+        self.search = self.driver.find_element(By.ID, "username")
+        self.search.send_keys(USERNAME)
+
+        self.search = self.driver.find_element(By.ID, "password")
+        self.search.send_keys(PASSWORD)
+        self.search.send_keys(Keys.ENTER)
+
+    def set_command_runner(self):
+        
+        self.driver.get("http://dash.townshiptale.com/servers/{}".format(SERVER_ID))
+        
+        command_runner_xpath = '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/form/div/div/input'
+        try:
+            self.command_runner = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, command_runner_xpath))
+            )
+        except e:
+            print(e)
 
 
-driver = webdriver.Chrome(PATH, options = options)
+    def get_player_detail(self, username):
+        player_detail_json_xpath = '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/code/p'
+        
+        if self.command_runner:
+            self.command_runner.send_keys("player detailed {}".format(username))
+            self.command_runner.send_keys(Keys.ENTER)
+            
+            try:
+                player_json = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, player_detail_json_xpath))
+                )
+            except e:
+                print(e)
+                return None
+                
+            return json.loads(player_json.text)
 
-driver.get("http://dash.townshiptale.com")
+    def write_to_csv(self, data, params, file):
+        
+        if('.csv' not in file[-4]):
+            return
+        
+        with open(file, 'r') as f:
+            
+            headers = next(f)
+            
 
-
-search = driver.find_element(By.ID, "username")
-search.send_keys(USERNAME)
-
-search = driver.find_element(By.ID, "password")
-search.send_keys(PASSWORD)
-search.send_keys(Keys.ENTER)
-
-sleep(5)
-
-driver.get("http://dash.townshiptale.com/servers/{}".format(SERVER_ID))
-
-sleep(5)
-
-#print(driver.current_url,'--------------------------')
-#print(driver.page_source)
 
 while True:
     sleep(3)
@@ -58,3 +108,12 @@ while True:
     player_json = json.loads(search.text)
     
     print("player {} is at chunk {}.".format(player_json['username'], player_json['Chunk']))
+    
+    
+    
+def main():
+    
+    print("asd")
+    
+if __name__ == '__main__':
+    main()
