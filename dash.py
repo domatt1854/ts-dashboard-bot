ce62b7daa1ce2bbeb0a4847eed59a989397dfe52
manylayers <domatthew1854@gmail.com>
2022-03-06 21:39:03 -0800

    Scrapes location and item data, need to deal with white screen issue

--- dash.py
+++ dash.py
@@ -3,129 +3,292 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
+from datetime import datetime
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-from secret import USERNAME, PASSWORD, SERVER_ID, HEADERS, PATH
+from secret import USERNAME, PASSWORD, SERVER_ID
+
 import json
-import requests
 
+import pandas as pd
+import re
 
-
-
-class TownshipLogger():
+class Quit:
+    
+    
     def __init__(self):
+        '''
+        init - initiates the web driver
+        '''
         
+        PATH = "C:\Program Files (x86)\chromedriver.exe"
+
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-        # put the PATH of your chromedriver here
-        self.driver = webdriver.Chrome(PATH, options = options)        
-        self.player_list = []
-        
-    # pings server to see if there are any active players
-    def ping_server(self):
-        
-        url = "https://967phuchye.execute-api.ap-southeast-2.amazonaws.com/prod/api/servers/2004963217"
-    
-        try:
-            response = requests.request("GET", url, headers=HEADERS, params={"limit":"20"})
-            
-            if(response.status_code == 200):
-                return response.json()
-        
-        except e:
-            print(e)
-            return None
-        
-    def authenticate(self):
+        self.driver = webdriver.Chrome(PATH, options = options)
 
+    def get(self, url, delay = 0):
+        '''
+        wrapper to navigate to a page and sleep if provided
+        '''
+        self.driver.get(url)
+        
+        if delay != 0:
+            sleep(delay)
+            
+
+    def auth(self):
+        '''
+        logs to the dashboard website and enters in user credentials
+        '''
+        
         self.driver.get("http://dash.townshiptale.com")
 
-        self.search = self.driver.find_element(By.ID, "username")
-        self.search.send_keys(USERNAME)
-
-        self.search = self.driver.find_element(By.ID, "password")
-        self.search.send_keys(PASSWORD)
-        self.search.send_keys(Keys.ENTER)
-
-    def set_command_runner(self):
-        
-        self.driver.get("http://dash.townshiptale.com/servers/{}".format(SERVER_ID))
-        
-        command_runner_xpath = '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/form/div/div/input'
         try:
-            self.command_runner = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, command_runner_xpath))
+            
+            self.username = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, "username"))
             )
-        except e:
-            print(e)
+            
+            self.username.send_keys(USERNAME)
+            
+            self.password = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, "password"))
+            )
+            
+            self.password.send_keys(PASSWORD)
+            self.password.send_keys(Keys.ENTER)
+            
+            sleep(3)
+            
+            self.driver.get("http://dash.townshiptale.com/servers/{}".format(SERVER_ID))
+            
+        except TimeoutException:
+            self.driver.refresh()
+            self.auth()
 
 
-    def get_player_detail(self, username):
-        player_detail_json_xpath = '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/code/p'
+
+    def send_command(self, command):
+        '''
+        locates the command runner position in the dashboard and inputs the provided
+        command into the command runner
+        '''
         
-        if self.command_runner:
-            self.command_runner.send_keys("player detailed {}".format(username))
+        try:
+            
+            self.command_runner = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/form/div/div/input'))
+            )
+            
+            self.command_runner.clear()
+            self.command_runner.send_keys(command)
             self.command_runner.send_keys(Keys.ENTER)
+            print("successfully sent command \'{}\'".format(command))
+            sleep(3)
             
-            try:
-                player_json = WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, player_detail_json_xpath))
-                )
-            except e:
-                print(e)
+            return 0
+        
+        except TimeoutException:
+            self.auth()
+    
+    def get_data(self, xpath):
+        
+        
+        try:
+            command_data = WebDriverWait(self.driver, 10).until(
+                        EC.presence_of_element_located((By.XPATH, xpath))
+            )
+            
+            if command_data is not None and command_data.text is not None:
+
+                return json.loads(command_data.text)
+            else:
                 return None
-                
-            return json.loads(player_json.text)
-
-    def write_to_csv(self, data, params, file):
         
-        if('.csv' not in file[-4:]):
-            print("Not a csv file...")
-            return
+        except TimeoutException:
+            return None
+        except json.JSONDecodeError:
+            return None
+    
+    def get_player_list_data(self):
         
-        with open(file, 'r') as f:
-            
-            headers = next(f)
-            
-            headers = headers.split(',')
-            
-            for i, header in enumerate(headers):
+        player_list = []
+        
+        for i in range(1, 9):
+        
+            xpath = '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/code/li[{}]'.format(i)
+        
+            try:
+                player_data = WebDriverWait(self.driver, 10).until(
+                            EC.presence_of_element_located((By.XPATH, xpath))
+                )
                 
-                if header != params[i]:
-                    print("improper parameter ordering.. expected: {}".format(headers))
-                    
+                player_list.append(json.loads(player_data.text))
+            
+            except TimeoutException:
+                return player_list
+    
+        return player_list
+        
+
+    def get_num_players(self):
+        
+        try:
+            xpath = '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[3]/div/div[1]/h3'
+            
+            search = self.driver.find_element(By.XPATH, xpath)
+            
+            result = search.text
+            
+            return result
+        except NoSuchElementException:
+            return None
+        
+    def check_login(self):
+        
+        try:
+            username = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.ID, "username"))
+            )
+            password = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.ID, "password"))
+            )
+
+            username.send_keys(USERNAME)
+            
+            password.send_keys(PASSWORD)
+            password.send_keys(Keys.ENTER)
+
+            sleep(3)
+            
+            self.driver.get("http://dash.townshiptale.com/servers/{}".format(SERVER_ID))
             
 
+            
+            print("logged in")
+            
+        except TimeoutException:
+            return 0
+    
+    def refresh(self):
+        self.driver.refresh()
+        
+            
 
-while True:
-    sleep(3)
-    search = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/form/div/div/input')
-    
-    player_name = "a player's name"
-
-    search.send_keys("player detailed {}".format(player_name))
-    search.send_keys(Keys.ENTER)
- 
-    sleep(3)
-    
-    #print(driver.page_source)
-    
-    search = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/code/p')
-    
-    print(search.text)
-    
-    
-    player_json = json.loads(search.text)
-    
-    print("player {} is at chunk {}.".format(player_json['username'], player_json['Chunk']))
-    
-    
-    
 def main():
     
-    print("asd")
+    seconds = 0
     
-if __name__ == '__main__':
+    bot = Quit()
+    
+    bot.auth()
+
+    
+    while(True):
+        response = bot.get_num_players()
+        
+        if(response and '0' not in response):
+            
+            bot.send_command("player list")
+            
+
+                        
+            data = bot.get_player_list_data()
+            now = datetime.now()
+            
+            
+            if(data):
+                for i, player in enumerate(data):
+
+
+                    current_time = now.strftime("%m/%d/%Y %H:%M:%S")
+
+
+                    print(i, ": ", player['username'])
+                    
+                    bot.send_command("player inventory {}".format(player['username']))
+                    
+                    
+                    data = bot.get_data('//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/code/li')
+                    
+                    player_items = []
+                    
+                    
+                    if data:
+                        
+                        
+                        for i in data['All']:
+                            
+                            item = i['Name']
+                            #print("appending ", item)
+                            
+                            
+                            item_name = item.replace('(Clone)', '')
+                            #print("after strip clonse", item_name)
+                            item_name = item_name.replace('-' , '')
+                           # print(item_name)
+                            item_name = item_name.replace(' ', '')
+                            #print(item_name)
+                            result = re.sub(r"\d+", "", item_name)
+                            #print("final: ", result)
+
+                            
+                            player_items.append(result)
+                    
+                    
+                            #print(result)
+                        
+                        location_xpath = '//*[@id="root"]/div/div/div[2]/div/div/div[2]/div[2]/div/div[2]/code/p'
+                        
+                        bot.send_command("player detailed {}".format(player['username']))
+                        
+                        location_data = bot.get_data(location_xpath)
+                        
+                        if location_data:
+                            location_data = location_data['Chunk']
+                        else:
+                            location_data = "None"
+                            
+                        df_dict = {
+                            'Time': current_time,
+                            'Username': player['username'],
+                            'Items': player_items,
+                            'Location': location_data
+                        }
+                        
+                        #print("player items: ", player_items)
+                        
+                        df = pd.DataFrame(df_dict)
+                        
+                        df.to_csv('item_locations.csv', mode = 'a', header = False)
+                        print("wrote to item_locations.csv")
+                        
+                            
+                        # line 381
+                    
+                
+        # tab this if it doesn't work later
+        print("checking login", seconds)
+        bot.check_login()
+            
+
+            #
+                # datetime object containing current date and time
+                # now = datetime.now()
+                
+                # print("now =", now)
+
+                # # dd/mm/YY H:M:S
+                # dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                # print("date and time =", dt_string)
+
+                
+                
+
+if __name__ == "__main__":
+    
     main()
