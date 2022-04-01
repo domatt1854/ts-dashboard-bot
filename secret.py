fc49fd6b90c54c859c3e0cc8d5514ab478654da6
domatt1854 <46241035+domatt1854@users.noreply.github.com>
2022-03-31 23:45:08 -0700

    Update secret.py

--- secret.py
+++ secret.py
@@ -4,8 +4,4 @@ USERNAME = environ['USERNAME']
 PASSWORD = environ['PASSWORD']
 SERVER_ID = environ['SERVER_ID']
 DISCORD_AUTH = environ['DISC_AUTH']
-<<<<<<< HEAD
 CHANNEL_ID = environ['CHANNEL_ID']
-=======
-CHANNEL_ID = environ['CHANNEL_ID']
->>>>>>> da9576701fbdd9586b5d154b2f6cfe13a9143e0e
