b1800a708612b1a6e1c001cab95d0b4699ed705a
domatt1854 <46241035+domatt1854@users.noreply.github.com>
2022-04-01 15:30:11 -0700

    Delete heroku_deploy directory

--- heroku_deploy/secret.py
+++
@@ -1,7 +0,0 @@
-from os import environ
-
-USERNAME = environ['USERNAME']
-PASSWORD = environ['PASSWORD']
-SERVER_ID = environ['SERVER_ID']
-DISCORD_AUTH = environ['DISC_AUTH']
-CHANNEL_ID = environ['CHANNEL_ID']
